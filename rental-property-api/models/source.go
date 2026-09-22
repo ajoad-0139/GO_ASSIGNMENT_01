bb5cf09b0976c049397b8704689d7f624d9bf8c2
Ajoad Islam <ajoadislam@gmail.com>
2026-09-22 11:47:40 +0600

    feat: source dto json-struct_object mapping structure created

---
+++ rental-property-api/models/source.go
@@ -0,0 +1,33 @@
+package models
+
+type RentalPropertiesDTO struct {
+	ID                 string   `json:"id"`
+	Feed               int      `json:"feed"`
+	Country            string   `json:"country"`
+	CountryCode        string   `json:"country_code"`
+	State              string   `json:"state"`
+	StateAbbr          string   `json:"state_abbr"`
+	City               string   `json:"city"`
+	Display            string   `json:"display"`
+	LocationID         string   `json:"location_id"`
+	PropertyName       string   `json:"property_name"`
+	PropertySlug       string   `json:"property_slug"`
+	PropertyType       string   `json:"property_type_category"`
+	USDPrice           float64  `json:"usd_price"`
+	Occupancy          int      `json:"occupancy"`
+	BedroomCount       int      `json:"bedroom_count"`
+	BathroomCount      int      `json:"bathroom_count"`
+	NumberOfReviews    int      `json:"number_of_review"`
+	ReviewScoreGeneral float64  `json:"review_score_general"`
+	StarRating         int      `json:"star_rating"`
+	AmenityCategories  []string `json:"amenity_categories"`
+	LonLat             LonLat   `json:"lonlat"`     // created a new type for nested key-value
+	Categories         string   `json:"categories"` //this field must be parsed as soon as the data is loaded in the memory
+	Published          bool     `json:"published"`
+	Images             []string `json:"images"`
+}
+
+// Nested key-value type
+type LonLat struct {
+	Coordinates []float64 `json:"coordinates"`
+}
---
+++ rental-property-api/services/property.go
@@ -0,0 +1,8 @@
+package services
+
+// import (
+// 	"encoding"
+// 	"errors/json"
+// 	"github.com/beego/beego/v2/core/logs"
+// 	"os"
+// )
