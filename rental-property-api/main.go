d38fa3123b11b7715d7017d48ad075e4668f20dc
Ajoad Islam <ajoadislam@gmail.com>
2026-09-22 13:46:20 +0600

    feat: load data in the memory at server startup

--- rental-property-api/main.go
+++ rental-property-api/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"rental-property-api/services"
+
 	_ "rental-property-api/routers"
+
+	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
+
+	// load data before starting the app
+	err := services.LoadData()
+	if err != nil {
+		logs.Critical("Failed to load data %v", err)
+		panic(err)
+	}
+
 	beego.Run()
 }
-
--- rental-property-api/services/property.go
+++ rental-property-api/services/property.go
@@ -1,8 +1,72 @@
 package services
 
-// import (
-// 	"encoding"
-// 	"errors/json"
-// 	"github.com/beego/beego/v2/core/logs"
-// 	"os"
-// )
+import (
+	"encoding/json"
+	"github.com/beego/beego/v2/core/logs"
+	"github.com/beego/beego/v2/server/web"
+	"os"
+	"rental-property-api/models"
+)
+
+type propertyStore struct {
+	isLoaded bool
+	data     []models.RentalPropertiesDTO
+}
+
+var propertyInstance propertyStore
+
+func LoadData() error {
+
+	// extract the source file path
+	sourcePath, err := web.AppConfig.String("sourcepath")
+
+	// check if path is valid or not
+	if err != nil {
+		logs.Critical("sourcepath is not configured in app.conf")
+		return err
+	}
+	if sourcePath == "" {
+		logs.Critical("sourcepath is not configured in app.conf")
+		return err
+	}
+
+	// read raw data using the source file path
+	rawData, err := os.ReadFile(sourcePath)
+	if err != nil {
+		logs.Error("failed to read sourcefile %v", err)
+		return err
+	}
+
+	// parse  raw json data on struct instance
+	var parsedData []models.RentalPropertiesDTO
+
+	parseErr := json.Unmarshal(rawData, &parsedData)
+	if parseErr != nil {
+		logs.Error("failed to parse raw json data : %v", err)
+		return err
+	}
+
+	// load data on memory
+
+	propertyInstance.data = parsedData
+	propertyInstance.isLoaded = true
+
+	return nil
+}
+
+// singleton object model and only way to get propertyStore instance
+// ensures both once data loading and encapsulation properties
+func GetPropertyStore() (*propertyStore, error) {
+	if !propertyInstance.isLoaded {
+		err := LoadData()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &propertyInstance, nil
+}
+
+// Get data from property store
+func (p *propertyStore) GetData() []models.RentalPropertiesDTO {
+	return p.data
+}
