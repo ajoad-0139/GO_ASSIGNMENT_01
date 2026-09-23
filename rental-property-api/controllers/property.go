3775ddd7ce7daafea1a362369baf70b713321d45
Ajoad Islam <ajoadislam@gmail.com>
2026-09-23 16:21:32 +0600

    update : changed error resoponse format, removed duplicates

--- rental-property-api/controllers/property.go
+++ rental-property-api/controllers/property.go
@@ -19,14 +19,14 @@ func (p *PropertyController) GetAllProperties() {
 	// checking and parsing test on filters
 	filter, errs := validators.ValidateAndParseFilter(&p.Controller)
 	if errs != nil {
-		utils.JsonError(&p.Controller, 400, map[string]interface{}{"Errors ": errs})
+		utils.JsonError(&p.Controller, 400, errs)
 		return
 	}
 
 	// take the property store
 	store, err := services.GetPropertyStore()
 	if err != nil {
-		utils.JsonError(&p.Controller, 500, map[string]interface{}{"Errors ": "failed to load property data"})
+		utils.JsonError(&p.Controller, 500, "failed to load property data")
 		return
 	}
 
@@ -41,21 +41,21 @@ func (p *PropertyController) GetAProperty() {
 	// get id params from request
 	id := p.Ctx.Input.Param(":id")
 	if id == "" {
-		utils.JsonError(&p.Controller, 400, map[string]interface{}{"Errors :": "id is not provided"})
+		utils.JsonError(&p.Controller, 400, "id is not provided")
 		return
 	}
 
 	// get property store
 	store, err := services.GetPropertyStore()
 	if err != nil {
-		utils.JsonError(&p.Controller, 500, map[string]interface{}{"Errors ": "failed to load property data"})
+		utils.JsonError(&p.Controller, 500, "failed to load property data")
 		return
 	}
 
 	// go to service layer and find property
 	property, found := services.GetAPropertyByID(store.GetData(), id)
 	if !found {
-		utils.JsonError(&p.Controller, 404, map[string]interface{}{"Errors ": "property not found"})
+		utils.JsonError(&p.Controller, 404, "property not found")
 		return
 	}
 	utils.JsonSuccess(&p.Controller, 200, *property)
